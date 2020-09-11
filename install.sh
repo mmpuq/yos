0d414049e7558b904c6a9ed8646a4a52989b8c27
mmpuq <44998325+mmpuq@users.noreply.github.com>
2020-09-11 19:35:07 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -26,7 +26,7 @@ EOF
 
 clone_repo(){
     echo "进行初始化。。。"
-    git clone https://github.com/mmpuq/yos
+    git clone https://github.com/mmpuq/yos.git
     cd IBMYes
     git submodule update --init --recursive
     cd gi-cloudfoundry/differentbi
